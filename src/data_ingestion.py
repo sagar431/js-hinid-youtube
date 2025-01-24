1b7342d351069683e5c99cd6bb1aa6eac8e8b39c
Sagar pallai <sagarpallai1997@gmail.com>
2025-01-24 06:54:08 +0000

    first experiment

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -2,11 +2,26 @@ import pandas as pd
 import numpy as np
 import os
 
-df = pd.read_csv('https://raw.githubusercontent.com/araj2/customer-database/master/Ecommerce%20Customers.csv')
+# Create data directory if it doesn't exist
+os.makedirs('data', exist_ok=True)
 
-df = df.iloc[:, 3:]
+try:
+    # Read data from URL
+    df = pd.read_csv('https://raw.githubusercontent.com/araj2/customer-database/master/Ecommerce%20Customers.csv')
 
-df = df[df['Length of Membership'] > 1]
+    # Select columns from index 3 onwards
+    df = df.iloc[:, 3:]
 
-df.drop(columns=['Avg. Session Length'],inplace=True)
-df.to_csv(os.path.join('data','customer.csv'))
+    # Filter rows where Length of Membership > 3
+    df = df[df['Length of Membership'] > 3]
+
+    # Drop specified column
+    df.drop(columns=['Avg. Session Length'], inplace=True)
+
+    # Save processed data
+    output_path = os.path.join('data', 'customer.csv')
+    df.to_csv(output_path, index=False)
+    print(f"Data successfully saved to {output_path}")
+
+except Exception as e:
+    print(f"Error occurred: {str(e)}")
